4bbf048c49b3c25b714f03f3431a34d8d27a5d1c
earl-stephens <stephensearl@hotmail.com>
2022-01-11 21:53:35 -0700

    Demonstrate use of static variables

--- src/application/App.java
+++ src/application/App.java
@@ -3,7 +3,21 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Cat cat1 = new Cat("Horace");
+		Cat cat2 = new Cat("Matilda");
+		
+		System.out.println(cat1.toString());
+		//System.out.println(cat1.FOOD);
+		//Gives an error about accessing the variable in a static way
+		
+		System.out.println(Cat.FOOD);
+		//now it is accessed via the class
+		
+		System.out.println(cat2.toString());
+		
+		//another example
+		System.out.println(Math.PI);
+		//Math is a class in Java, PI is a Math static variable
 
 	}
 
